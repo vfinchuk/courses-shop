18a3f0860d987e5ff2d72fb75cb28d6eb4f78422
vfinchuk <finchukvladimir@gmail.com>
2020-08-15 13:29:50 +0300

    Finish cart order

---
+++ models/order.js
@@ -0,0 +1,32 @@
+const {Schema, model} = require('mongoose')
+
+const ordersSchema = new Schema({
+  courses: [
+    {
+      course: {
+        type: Object,
+        required: true,
+      },
+      count: {
+        type: Number,
+        required: true,
+      }
+    }
+  ],
+  user: {
+    name: {
+      type: String,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = model('Order', ordersSchema)
